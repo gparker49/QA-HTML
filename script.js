58d77fe025b212e37f0147dbfcc87c53f77b5dd6
gparker49 <georgeparker60@hotmail.com>
2021-10-12 11:56:54 +0100

    completed first exercises

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("George");
+console.info("Parker");
+console.warn("Leicester");
+console.error("Scorpio");
+
+let make = "Audi";
+let model = "TT";
+
+console.log("My favourite car is", make, " and the model is ", model);
+
+console.log("%cHello World!", "color: orange; font-family: fantasy; font-style: bold; background-color: black; padding: 10px" );
